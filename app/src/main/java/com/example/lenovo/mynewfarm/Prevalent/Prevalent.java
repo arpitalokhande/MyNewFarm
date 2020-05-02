7b33afe56ad4d42c7faab969f8779dcc664d2fd4
Arpita <arpitalokhande100@gmail.com>
2020-05-02 23:28:53 +0530

    abcd

---
+++ app/src/main/java/com/example/lenovo/mynewfarm/Prevalent/Prevalent.java
@@ -0,0 +1,11 @@
+package com.example.lenovo.mynewfarm.Prevalent;
+
+import com.example.lenovo.mynewfarm.Model.Users;
+
+public class Prevalent
+{
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
